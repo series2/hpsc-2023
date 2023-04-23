dac14262022e75d0c6dd432c32f00d803d0438d2
series2 <series2.others@gmail.com>
2023-04-24 00:15:59 +0900

    kadai3

--- 02_openmp/14_bucket_sort.cpp
+++ 02_openmp/14_bucket_sort.cpp
@@ -13,16 +13,43 @@ int main() {
   printf("\n");
 
   std::vector<int> bucket(range,0); 
-  for (int i=0; i<n; i++)
+#pragma omp parallel for shared(bucket)
+  for (int i=0; i<n; i++){
+#pragma omp atomic update
     bucket[key[i]]++;
+  }
   std::vector<int> offset(range,0);
-  for (int i=1; i<range; i++) 
-    offset[i] = offset[i-1] + bucket[i-1];
+//   for (int i=1; i<range; i++) 
+    // offset[i] = offset[i-1] + bucket[i-1];
+/*   
+/  offset[i]= offset[i-1] + bucket[i-1] (1<=i<range)
+/  offset[0]=0
+/  then offset[n]-offset[0] = \sum_{i=1}^{n} bucket[i-1] (1<=i<range)
+/  then offset[n] = \sum_{i=0}^{n-1} bucket[i](1<=i<range)
+*/
+  std::vector<int> temp(range,0);
+#pragma omp parallel for
+  for(int i=1; i<range; i++){
+    offset[i] = bucket[i-1];
+  }
+#pragma omp parallel
+  for(int j=1; j<range; j<<=1) {
+#pragma omp for
+    for(int i=0; i<range; i++)
+      temp[i] = offset[i];
+#pragma omp for
+    for(int i=j; i<range; i++)
+      offset[i] += temp[i-j];
+  }
+
+#pragma omp parallel for
   for (int i=0; i<range; i++) {
     int j = offset[i];
-    for (; bucket[i]>0; bucket[i]--) {
+#pragma omp parallel for
+    for (int k=0; k<bucket[i]; k++) {
       key[j++] = i;
     }
+    bucket[i]=0;
   }
 
   for (int i=0; i<n; i++) {
